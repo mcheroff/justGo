1b2dbbabf8d8f233f3aa3c6bd7f6295c6e2f00e5
Benjamin Arteaga <bnjmn_rtg@yahoo.com>
2016-08-06 11:50:56 -0500

    api token

--- server.js
+++ server.js
@@ -4,21 +4,43 @@ var bodyParser = require('body-parser');
 var request = require('request');
 
 var PORT = 3000;
- // process.env.PORT || 
+// process.env.PORT || 
+var options = {
+    method: 'POST',
+    url: 'https://ws.homeaway.com/oauth/token',
+    headers: {
+        'postman-token': '940e960b-01e8-9a90-b5ca-130cfe8d4c26',
+        'cache-control': 'no-cache',
+        authorization: 'Basic Y2QyZGQyM2MtYmUwMy00OTk4LTg4NWItYjg0ZDY4ODg4ZjEyOmU4ZjcyZjUzLTRlMTMtNDYyNy04NDQ4LTBhYjQzNDVlYWYyMw=='
+    }
+};
+
+var options2 = {
+    method: 'GET',
+    url: 'https://ws.homeaway.com/public/search',
+    qs: { minSleeps: '3', q: 'london', maxNightlyPrice: '300' },
+    headers: {
+        'postman-token': '6c3a6488-22a3-b3df-3842-a7571a4fac34',
+        'cache-control': 'no-cache',
+        authorization: 'Bearer NTZlNjYzZGYtNTYxNS00NWViLWFjZTQtOWY0ZDVlMmMwZjIz'
+    }
+};
 
 var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req,res,next) {
-    request('https://ws.homeaway.com/oauth/token', function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(response, body)
-      }
-  	})
+app.get('/', function(req, res, next) {
+    request(options, function(error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+    })
+
+
 });
 
-app.listen(PORT, function(){
+app.listen(PORT, function() {
     console.log('Listening on: ' + PORT);
 });
