951f846f4d18675ef5b68edbc17b1a55b2c261b4
James Hornung <hornung.james@gmail.com>
2016-08-07 08:31:17 -0500

    handlebars displaying the splash page and linked to javascript and css files

--- public/assets/js/background.js
+++ public/assets/js/background.js
@@ -3,11 +3,11 @@
     var inum = 5;
     var rand1 = Math.round(randnum * (inum - 1)) + 1;
     images = new Array
-    images[1] = "../public/images/bg/1.jpg"
-    images[2] = "../public/images/bg/2.jpg"
-    images[3] = "../public/images/bg/3.jpg"
-    images[4] = "../public/images/bg/4.jpg"
-    images[5] = "../public/images/bg/5.jpg"
+    images[1] = "assets/images/bg/1.jpg"
+    images[2] = "assets/images/bg/2.jpg"
+    images[3] = "assets/images/bg/3.jpg"
+    images[4] = "assets/images/bg/4.jpg"
+    images[5] = "assets/images/bg/5.jpg"
     var image = images[rand1]
 // Deactivate cloaking device -->
 
